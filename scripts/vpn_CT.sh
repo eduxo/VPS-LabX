51fc12a27dc36b74d8b43bc420b9e57807f0da6b
Mr.Eduxo <mr@eduxo.cz>
2023-10-06 11:07:00 +0200

    Update vpn_CT.sh

--- scripts/vpn_CT.sh
+++ scripts/vpn_CT.sh
@@ -23,8 +23,8 @@ fi
 # Pripojeni do VPN site VPS-LabX
 echo -e '\n\e[0;92mPripojuji k VPN siti VPS-LabX\e[0m'
 
-sudo zerotier-cli join c075fcef7ece6a93
-sudo zerotier-cli set c075fcef7ece6a93 allowDNS=1
+sudo zerotier-cli join c075fcef7e01120e
+sudo zerotier-cli set c075fcef7e01120e allowDNS=1
 sudo zerotier-cli status
 sleep 5
 
@@ -58,10 +58,9 @@ iface vmbr1 inet static
         bridge-stp off
         bridge-fd 0
         bridge-maxwait 3
-        post-up echo \"1\" > /proc/sys/net/ipv4/ip_forward
+        post-up echo 1 > /proc/sys/net/ipv4/ip_forward
         post-up iptables -t nat -A POSTROUTING -s '\''10.20.30.0/24'\'' -o vmbr0 -j MASQUERADE
         post-down iptables -t nat -D POSTROUTING -s '\''10.20.30.0/24'\'' -o vmbr0 -j MASQUERADE
-
 " >> /etc/network/interfaces'
 
     break
