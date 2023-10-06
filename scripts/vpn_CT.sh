e4c46faa122fad2584712122ee5e88868f9a8a8f
Mr.Eduxo <mr@eduxo.cz>
2023-10-06 10:25:58 +0200

    Update vpn_CT.sh

--- scripts/vpn_CT.sh
+++ scripts/vpn_CT.sh
@@ -57,7 +57,7 @@ iface vmbr1 inet static
         bridge-ports '$interface'
         bridge-stp off
         bridge-fd 0
-
+        bridge-maxwait 3
         post-up echo \"1\" > /proc/sys/net/ipv4/ip_forward
         post-up iptables -t nat -A POSTROUTING -s '\''10.20.30.0/24'\'' -o vmbr0 -j MASQUERADE
         post-down iptables -t nat -D POSTROUTING -s '\''10.20.30.0/24'\'' -o vmbr0 -j MASQUERADE
