952859ef8635696c2010d12d2fc300190abf3a6d
Mr.Eduxo <mr@eduxo.cz>
2023-10-06 08:20:43 +0200

    Update vpn_CT.sh

--- scripts/vpn_CT.sh
+++ scripts/vpn_CT.sh
@@ -14,9 +14,9 @@ function check_internet() {
 
 check_internet
 
-# Test, jestli je ZeroTier nainstalovany, pokud ne, nainstaluje se
+# test, jestli je ZeroTier nainstalovany, pokud ne, nainstaluje se
 if ! dpkg --get-selections | grep -qw zerotier;then
-    echo -e '\n\e[0;92mZeroTier neni nainstalovan. Instaluji ZeroTier.\e[0m'
+    echo -e '\n\e[0;92mZeroTier neni nanistalovan. Instaluji ZeroTier.\e[0m'
     curl -s https://install.zerotier.com | sudo bash
 fi
 
@@ -26,10 +26,10 @@ echo -e '\n\e[0;92mPripojuji k VPN siti VPS-LabX\e[0m'
 sudo zerotier-cli join c075fcef7ece6a93
 sudo zerotier-cli set c075fcef7ece6a93 allowDNS=1
 sudo zerotier-cli status
-sleep 5
+sudo zerotier-cli listnetworks
+sleep 3
 
-
-# Pridani zaznamu do /etc/network/interfaces
+# Pridani zaznamu do /etc/network/interfaces z duvodu pouziti pro CT
 
 # Ziskani seznamu sitovych rozhrani
 network_interfaces=$(ip -o link show | awk -F': ' '{print $2}')
@@ -37,36 +37,24 @@ network_interfaces=$(ip -o link show | awk -F': ' '{print $2}')
 # Projde seznam a najde zacinajici na "zt"
 for interface in $network_interfaces; do
   if [[ $interface == zt* ]]; then
-    # Ziskani IP adresy a masky pro ZT rozhrani
-    ip_info=$(ip -o -f inet addr show dev $interface | awk '{print $4}')
-
-    # Extrahujte IP adresu a masku ze ziskanych informaci
-    ip_address=$(echo $ip_info | cut -d'/' -f1)
-    cidr_mask=$(echo $ip_info | cut -d'/' -f2)
-
-    # Vytvori CIDR notaci
-    cidr_notation="$ip_address/$cidr_mask"
-
-sudo sh -c 'echo "
-# Interface for ZeroTier
-iface '$interface' inet manual
-
-auto vmbr1
-iface vmbr1 inet static
-	address '$cidr_notation'
-        bridge-ports '$interface'
-        bridge-stp off
-        bridge-fd 0
-        bridge-maxwait 3
-        post-up   echo 1 > /proc/sys/net/ipv4/ip_forward
-        post-up   iptables -t nat -A POSTROUTING -s '\''10.20.30.0/24'\'' -o vmbr0 -j MASQUERADE
-        post-down iptables -t nat -D POSTROUTING -s '\''10.20.30.0/24'\'' -o vmbr0 -j MASQUERADE
-" >> /etc/network/interfaces'
-
+    sudo sh -c 'echo "
+    
+    iface $interface inet manual
+    
+    auto vmbr1
+    iface vmbr1 inet dhcp
+            bridge-ports $interface
+            bridge-stp off
+            bridge-fd 0
+            bridge-maxwait 3
+            post-up   echo 1 > /proc/sys/net/ipv4/ip_forward
+            post-up   iptables -t nat -A POSTROUTING -s '10.20.30.0/24' -o vmbr0 -j MASQUERADE
+            post-down iptables -t nat -D POSTROUTING -s '10.20.30.0/24' -o vmbr0 -j MASQUERADE
+    " >> /etc/network/interfaces'
     break
   fi
 done
 
-echo -e '\n\e[0;92mHotovo. Restartuji systemu.\e[0m'
+echo -e '\e[1;92mHotovo, system bude restartovan!\e[0m'
 sleep 3
 sudo reboot
